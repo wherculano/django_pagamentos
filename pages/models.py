ff13937ae0db95709f1aa86d195b0c5fa752538c
wagner <wagherculano@hotmail.com>
2021-10-16 15:13:58 -0300

    Implemented Client and Bills Models

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    company_name = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=18)
+
+
+class Bills(models.Model):
+    issue_date = models.DateField()
+    due_date = models.DateField()
+    value = models.DecimalField(max_digits=12, decimal_places=2)
+    referred_company = models.ForeignKey('Client', on_delete=models.CASCADE)
+
